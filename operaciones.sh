96cdd1c119e64ea38b8793e31e372ae308d679f1
Alex Milano <alexjmilano@gmail.com>
2023-01-25 12:32:01 -0300

    Suma, resta, multiplica y divide

---
+++ operaciones.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+read -p "Introduzca el primer numero:" n1
+read -p "Introduzca el segundo numero:" n2
+echo "La suma es $(($n1+$n2))"
+echo "La resta es $(($n1-$n2))"
+echo "La multiplicacion es $(($n1*$n2))"
+echo "La division es $(($n1/$n2))" 
